121fa3eb76b5fb742150e62fb4e12d023e1115a9
wassimmho <wassim.mho000@gmail.com>
2025-03-23 17:54:58 +0100

    some changes

--- my-react-app/src/Login.jsx
+++ my-react-app/src/Login.jsx
@@ -1,37 +1,44 @@
 import './Login.css'
 import React from 'react'
 import logo from './assets/Group 1.png'
+import topright from './assets/topright.png';
+import bottomleft from './assets/bottomleft.png';
+import bottomright from './assets/bottomright.png';
+//import bottomright2 from './assets/Vector 2.png';
+
 function Login(){
 
 
-    return<>
+    return <>
     <div className="container">
       <div id="left">
         <div>
-            <img src={logo} alt="Logo" />
+            <img id="logo" src={logo} alt="Logo" />
         </div>
+        
+            <img id="topright" src={topright} alt="" />
+            <img id="bottomleft" src={bottomleft} alt="" />
+            <img id="bottomright" src={bottomright} alt="" />
       </div>
       <div id="right">
-        <h1>Title</h1>
-        <p>Description</p>
-        <hr />
+        <h1>Welcome to Scope</h1>
+        <p id='discription'>SCOPE allows professors to securely swap class slots with mutual approval, ensuring transparency, minimizing conflicts, and providing real-time updates.</p>
         <input type="text" placeholder="Enter Name..." />
         <input type="text" placeholder="Enter username..." />
         <input type="text" placeholder="Enter Email..." />
+        <div id='password'>   
         <input type="text" placeholder="Enter Password..." />
         <p>
           <a href="#" className="textbut">Forgot Password?</a>
         </p>
-        <button>Submit</button>
-        <button>Cancel</button>
-        <p>
+        </div>
+        
+        <button type="submit" id='submit'>Login</button>
+        <p >
         Can't sign in? Try <a href="#" className="textbut">resetting your password</a> or <a href="#" className="textbut">contact us</a> for assistance.
         </p>
       </div>
     </div>
-    
-    
-
     </>
 }
 
