2f2b7f78f2a1365e70e75fee9a49544afaefe70c
wassimmho <wassim.mho000@gmail.com>
2025-04-08 01:02:39 +0100

    add FAQ component

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Feature from './website/Features.jsx'
+import FAQ from './website/FAQ.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Feature />
+    <FAQ />
     
   </StrictMode>,
 )
---
+++ Frontend/src/website/FAQ.jsx
@@ -0,0 +1,40 @@
+import './faq.css'
+import arrow from'../assets/arrow.png'
+
+function FAQ() {
+
+    return<>
+
+        <div className='container' id='faq'>
+            <div className='card'>
+                <div id='text'>
+                    <h1>You ask - we answer</h1>
+                    <p>All you want to know about Scope</p>
+                </div>
+
+                <div id='questions'>
+                    <div>
+                        <p>How do I create an account?</p>
+                        <img src={arrow} alt='button'/>
+                    </div>
+                    <hr />
+                    <div>
+                        <p>Where can I get help if I encounter an issue?</p>
+                        <img src={arrow} alt='button'/>
+                    </div>
+                    <hr />
+                    <div>
+                        <p>What is the purpose of this platform?</p>
+                        <img src={arrow} alt='button'/>
+                    </div>
+                    <hr />
+                    <div>
+                        <p>How do I request a class swap?</p>
+                        <img src={arrow} alt='button'/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
+}
+export default FAQ;
